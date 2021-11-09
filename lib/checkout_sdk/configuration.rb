3ec6e2656e2953fc0ae6eff61953519481df4255
Duncan Robertson <duncan@kyan.com>
2021-11-09 13:59:38 +0000

    Allow persistent option to be configurable

--- lib/checkout_sdk/configuration.rb
+++ lib/checkout_sdk/configuration.rb
@@ -1,9 +1,10 @@
 class CheckoutSdk::Configuration
-  attr_accessor :secret_key, :public_key, :base_url
+  attr_accessor :secret_key, :public_key, :base_url, :persistent
 
   def initialize
     @secret_key = nil
     @public_key = nil
     @base_url = nil
+    @persistent = true
   end
 end
--- spec/checkout_sdk_spec.rb
+++ spec/checkout_sdk_spec.rb
@@ -4,4 +4,18 @@ RSpec.describe CheckoutSdk do
   it "has a version number" do
     expect(CheckoutSdk::VERSION).not_to be nil
   end
+
+  describe "Config persistent" do
+    it "has a default persistent option" do
+      expect(CheckoutSdk.configuration.persistent).to be_truthy
+    end
+
+    it "allows you to change the persistent option" do
+      CheckoutSdk.configure do |config|
+        config.persistent = false
+      end
+
+      expect(CheckoutSdk.configuration.persistent).to be_falsy
+    end
+  end
 end
