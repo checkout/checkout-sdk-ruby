4f55f0065d13c62c6b0fe225bb18cd85e33893da
Martin Seco <mseco.93@gmail.com>
2022-09-30 12:46:09 +0200

    Adds support for Customers (CS1)

--- lib/checkout_sdk/previous/checkout_api.rb
+++ lib/checkout_sdk/previous/checkout_api.rb
@@ -2,17 +2,21 @@
 
 module CheckoutSdk
   module Previous
+    # @!attribute customers
+    #   @return [CheckoutSdk::Customers::CustomerClient]
     # @!attribute klarna
     #   @return [CheckoutSdk::Previous::Apm::KlarnaClient]
     # @!attribute sepa
-    #   @return [CheckoutSdk::Previous::Apm::Sepa::SepaClient]
+    #   @return [CheckoutSdk::Previous::Apm::SepaClient]
     class CheckoutApi
-      attr_reader :klarna,
+      attr_reader :customers,
+                  :klarna,
                   :sepa
 
       # @param [CheckoutConfiguration] configuration
       def initialize(configuration)
         api_client = base_api_client configuration
+        @customers = CheckoutSdk::Customers::CustomersClient.new api_client, configuration
         @klarna = CheckoutSdk::Previous::Apm::KlarnaClient.new api_client, configuration
         @sepa = CheckoutSdk::Previous::Apm::SepaClient.new api_client, configuration
       end
