5d3eee1c1d5b8b5cd0bb51ced98dc72f16ff9fda
Armando Rodríguez <armando.rodriguez@checkout.com>
2023-05-11 10:54:49 +0200

    GTW-9660: Add authentication_status_reason field to 3ds object

--- spec/checkout_sdk/payments/get_payment_details_integration_spec.rb
+++ spec/checkout_sdk/payments/get_payment_details_integration_spec.rb
@@ -31,4 +31,35 @@ RSpec.describe CheckoutSdk::Payments do
       end
     end
   end
+
+  describe '.get_payment_details' do
+    context 'make 3ds card payment n3d' do
+      it 'returns payment details' do
+        payment_response = make_3ds_card_payment(attempt_n3d: true)
+
+        response = @api.payments.get_payment_details(payment_response.id)
+        assert_response(response, %w[id
+                                     requested_on
+                                     source.id
+                                     source.type
+                                     source.fingerprint
+                                     source.card_type
+                                     amount
+                                     currency
+                                     payment_type
+                                     reference
+                                     status
+                                     approved
+                                     scheme_id
+                                     customer.id
+                                     customer.name
+                                     sender.address.city
+                                     3ds.downgraded
+                                     3ds.cryptogram
+                                     3ds.xid
+                                     3ds.version])
+
+      end
+    end
+  end
 end
