215ad257ccf1cdb14d0b08dbb989516d2de68647
Armando Rodríguez <127134616+armando-rodriguez-cko@users.noreply.github.com>
2024-03-19 11:03:34 +0100

    Update non hosted completion properties

--- lib/checkout_sdk/sessions/completion/non_hosted_completion_info.rb
+++ lib/checkout_sdk/sessions/completion/non_hosted_completion_info.rb
@@ -4,8 +4,11 @@ module CheckoutSdk
   module Sessions
     # @!attribute callback_url
     #   @return [String]
+    # @!attribute challenge_notification_url
+    #     #   @return [String]
     class NonHostedCompletionInfo < CompletionInfo
-      attr_accessor :callback_url
+      attr_accessor :callback_url,
+                    :challenge_notification_url
 
       def initialize
         super CompletionInfoType::NON_HOSTED
