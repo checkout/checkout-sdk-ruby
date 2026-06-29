4c39ba9e0190550945cf50158aeeb81989867a9a
david ruiz <david.ruiz@checkout.com>
2026-06-29 11:11:39 +0200

    feature/INT-1637 - Payment Request Product Type and Subtype to nullable

---
+++ lib/checkout_sdk/payments/product_item_type.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module CheckoutSdk
+  module Payments
+    module ProductItemType
+      DIGITAL = 'digital'
+      DISCOUNT = 'discount'
+      PHYSICAL = 'physical'
+    end
+  end
+end
